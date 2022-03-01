19b2926ff8ce7af9d4239573e9d0902f079b39c7
John Smith <rintran720@gmail.com>
2022-03-01 09:04:41 +0700

    chore: Session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(
+    session({ secret: 'my-secret', resave: false, saveUninitialized: false }),
+  );
   await app.listen(3000);
 }
 bootstrap();
